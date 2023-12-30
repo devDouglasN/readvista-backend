028374c5553fc372745f7f7e63c7781282382cf4
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2023-12-30 15:20:19 -0300

    Initial commit

---
+++ src/test/java/com/douglas/readvista/ReadVistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.readvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadVistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
