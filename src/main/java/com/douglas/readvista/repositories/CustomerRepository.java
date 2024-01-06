a8c1ecb66658d7abe702a41c308812955d2c49dd
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-06 16:52:00 -0300

    Custom exception for duplicate CPF and email

--- src/main/java/com/douglas/readvista/repositories/CustomerRepository.java
+++ src/main/java/com/douglas/readvista/repositories/CustomerRepository.java
@@ -1,8 +1,14 @@
 package com.douglas.readvista.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.douglas.readvista.entities.Customer;
 
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+	
+	Optional<Customer> findByCpf(String cpf);
+	Optional<Customer> findByEmail(String email);
 }
--- src/main/java/com/douglas/readvista/services/CustomerService.java
+++ src/main/java/com/douglas/readvista/services/CustomerService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.douglas.readvista.dtos.CustomerDTO;
 import com.douglas.readvista.entities.Customer;
 import com.douglas.readvista.repositories.CustomerRepository;
+import com.douglas.readvista.services.exceptions.DataIntegrityViolationException;
 import com.douglas.readvista.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,7 +29,21 @@ public class CustomerService {
 
 	public Customer create(CustomerDTO objDTO) {
 		objDTO.setId(null);
+		validationCpfAndEmail(objDTO);
 		Customer newObj = new Customer(objDTO);
 		return customerRepository.save(newObj);
+	}
+
+	public void validationCpfAndEmail(CustomerDTO objDTO) {
+		Optional<Customer> obj = customerRepository.findByCpf(objDTO.getCpf());
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF already registered in the system!");
+		}
+
+		obj = customerRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email already registered in the system!");
+		}
 	}
 }
