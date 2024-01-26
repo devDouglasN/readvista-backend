bbcf5bb735556bf1fbffd8d7febecfaf5c2114b4
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-26 17:02:39 -0300

    Book availability validator

---
+++ src/main/java/com/douglas/readvista/loan/validators/BookAvailabilityValidator.java
@@ -0,0 +1,24 @@
+package com.douglas.readvista.loan.validators;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.douglas.readvista.repositories.BookRepository;
+import com.douglas.readvista.services.exceptions.ValidationException;
+
+@Component
+public class BookAvailabilityValidator {
+	
+	@Autowired
+	private BookRepository repository;
+	
+	public void validator(BookLoanData data) {
+		
+		var idBook = repository.existsByIdAndActiveTrue(data.idBook());
+		
+		if(!idBook) {
+			throw new ValidationException("Book not available!");
+		}
+	}
+	
+}
--- src/main/java/com/douglas/readvista/repositories/BookRepository.java
+++ src/main/java/com/douglas/readvista/repositories/BookRepository.java
@@ -1,8 +1,17 @@
 package com.douglas.readvista.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.douglas.readvista.entities.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer>{
+
+	@Query("""
+			select b.active
+			from Book b
+			where
+			b.id = :id
+			""")
+	boolean existsByIdAndActiveTrue(Integer id);
 }
